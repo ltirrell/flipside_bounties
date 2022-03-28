3cf90afd9d1c7585700d02d176ca02f803e22683
Lee Tirrell <tirrell.le@gmail.com>
2022-03-28 23:14:39 +0000

    add landing page

---
+++ terra/luna_report.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.set_page_config(page_title="LUNA Report", page_icon="🌕")
+st.title("Welcome to the LUNA Report!")
+st.caption("Created by [@ltirrell_](https://twitter.com/ltirrell_)")
+
+"""
+The LUNA Report provides data-driven dashboards for topics relevant to the Terra/LUNA community!
+
+Currently, these are available:
+- [LUNAr Lander](https://luna.report/lander): High-level overview of blockchain metrics, UST Peg stability, user metrics, and other information.
+- [LFG!](https://luna.report/lfg): Tracking the Luna Foundation Guard, its reserves (in BTC, LUNA and other currency), and relevant news.
+
+Reach out to me on [Twitter](https://twitter.com/ltirrell_) for more info, comments or requests!
+"""
+
