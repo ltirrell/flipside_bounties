d3bee0d8501e508410c80d9a0fd744b419b07c7d
Lee Tirrell <tirrell.le@gmail.com>
2022-07-06 16:33:14 -0700

    Initial NEAR commit

---
+++ near/citizens_of_NEAR_users.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Citizens of NEAR: Active USers", page_icon="🅰️")
+st.title("Citizens of NEAR: Active Users")
+st.caption(
+    """
+ If NEAR were a city, who would the citizens be?
+
+ Let's look at the active users of NEAR, and examine any trends or outliers in the last 90 days
+"""
+)
+
+st.header("NOTE: WORK IN PROGRESS")
+"Note that this is a work in progress, and will be updated shortly"
