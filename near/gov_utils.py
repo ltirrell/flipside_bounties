88c73991fe128be550391cb79d6df6a31f7a57bf
Lee Tirrell <tirrell.le@gmail.com>
2022-08-03 21:27:53 -0700

    more debug

--- near/gov_utils.py
+++ near/gov_utils.py
@@ -91,7 +91,7 @@ def get_validators() -> pd.DataFrame:
     url = f"{fig_url}/validators"
     r = requests.get(url)
     data = r.json()
-    logging.info(r)
+    return data
     df = pd.DataFrame(data)
     # df = df[df.active == True].reset_index(drop=True) # NOTE: this is causing issues for some reason
     df["last_time"] = pd.to_datetime(df["last_time"])
--- near/government.py
+++ near/government.py
@@ -40,7 +40,9 @@ try:
     validator_names = validators.account_id
     total_staked = validators["Stake (NEAR)"].sum()
     validators
-    logging.log(fig_key)
+    blocktimes
+    status
+
     vals_sorted_stake = validators.sort_values(
         by="Stake (NEAR)", ascending=False
     ).reset_index(drop=True)
